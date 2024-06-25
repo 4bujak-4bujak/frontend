1a34a4b03aa39650ff484957374e3d5d38c826bc
eunhak <akfmzh1207@naver.com>
2024-06-25 20:31:56 +0900

    feat: jest 설정

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,16 @@
 import withPWA from 'next-pwa';
 
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/sign',
+        permanent: true // true로 설정하면 308 리다이렉션, false로 설정하면 307 리다이렉션
+      }
+    ];
+  },
+
   images: {
     domains: [
       'bzbz-file-bucket.s3.ap-northeast-2.amazonaws.com',
